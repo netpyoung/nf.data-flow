64c5973fff5b2e5142aa031ba811bb582ad06115
netpyoung <netpyoung@gmail.com>
2022-06-06 01:01:46 +0900

    chore: modify project settings

--- solution/DataFlowGUI/Program.cs
+++ solution/DataFlowGUI/Program.cs
@@ -14,9 +14,7 @@ namespace DataFlowGUI
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            //ApplicationConfiguration.Initialize();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            ApplicationConfiguration.Initialize();
             Application.Run(new DataFlowGUIForm());
         }
     }
